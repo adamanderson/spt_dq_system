9e4d509d35f0f1ca0133f745d2d9959d08502294
Adam Anderson <adama@fnal.gov>
2017-04-28 18:54:35 -0500

    Script for building the file database

---
+++ build_database.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+import os
+
+data_dir = 
+
+client = MongoClient('localhost', 27017)
+db = client.file_db
+
+
+files = os.listdir(os.getcwd())
+print files
+
+conn = sqlite3.connect('filelist.db')
+c = conn.cursor()
+c.execute('create table filetable(id integer primary key, one varchar(100), two info);')
+
+for fname in files:
+    c.execute('insert into filetable values(null, \'{}\', 10);'.format(fname))
+# c.execute()
+conn.commit()
+c.execute('select * from filetable;')
+print c.fetchall()
