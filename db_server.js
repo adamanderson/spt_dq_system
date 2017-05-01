27fb9bff9077ab7ec6dec18ba4156478fe1f3787
Adam Anderson <adama@fnal.gov>
2017-04-30 23:05:33 -0500

    Add body-parser for sending requests from client to server

--- db_server.js
+++ db_server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
 var assert = require('assert');
+var bodyParser = require('body-parser');
 
 // open the database
 db = new sqlite3.Database('obsfiles.db');
 
 // start the server
 var app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 app.get('/index.html', function (req, res) {
@@ -14,7 +17,7 @@ app.get('/index.html', function (req, res) {
 })
 
 app.get('/all', function(req, res) {
-    // query the database with an example query
+    // get all data from the database
     db.all('SELECT * FROM obsfiles;', function(err, rows) {
 	assert.equal(null, err);
 	console.log('querying all rows from observation file table...')
