61e8907b34312f1428a23ca98420c856115dc67d
Adam Anderson <adama@scott.grid.uchicago.edu>
2017-05-09 19:48:43 -0500

    Build database from Sasha's file transfer stuff.

--- build_database.py
+++ build_database.py
@@ -1,27 +1,52 @@
 import sqlite3
 import os
 import glob
+import pandas as pd
+import numpy as np
 
-data_dir = '/spt/data/bolodata/downsampled/'
-source_types = ['calibrator', 'CenA', 'elnod', 'RCW38', 'saturn']
-dbname = 'obsfiles'
+dbname = 'master_database'
+
+# transfer database files
+transfer_path = '/spt/data/transfer_database/'
+transfer_file = '{}/transfer.txt'.format(transfer_path)
+
 dbfile = '{}.db'.format(dbname)
 
 # delete existing database file
 if os.path.isfile(dbfile):
     os.remove(dbfile)
 
+typestr = {np.dtype('O'): 'text',
+            np.dtype('int64'): 'int'}
+
+# fill the database
+df_transfer = pd.read_csv(transfer_file, sep='\t',
+                            dtype={'source': np.str,
+                                    'observation': np.int,
+                                    'status_fullrate': np.str,
+                                    'status_downsampled': np.str,
+                                    'transfer_fullrate': np.int,
+                                    'transfer_downsampled': np.int})
+df_transfer.fillna('NULL', inplace=True)
+def generate_path(row):
+    path = '/spt/data/bolodata/downsampled/{}/{}/' \
+            .format(df_transfer['source'], df_transfer['observation'])
+    if os.path.isdir(path):
+        return path
+    else:
+        return 'none'
+df_transfer['path'] = df_transfer.apply(lambda row: generate_path(row), axis=1)
+
 # create database and table
 conn = sqlite3.connect(dbfile)
 c = conn.cursor()
-c.execute('CREATE TABLE {} (path text, type text, id int)'.format(dbname))
+column_schema = ', '.join(['{} {}'.format(var, typestr[df_transfer[var].dtype]) for var in df_transfer.columns])
+c.execute('CREATE TABLE {} ({})'.format(dbname, column_schema))
 
-# fill the database
-for source in source_types:
-    files = glob.glob('{}/{}/*'.format(data_dir, source))
-    for fname in files:
-        c.execute("INSERT INTO {} VALUES ('{}', '{}', {})" \
-                  .format(dbname, os.path.abspath(fname), source, int(os.path.basename(fname))))
+for index, row in df_transfer.iterrows():
+    c.execute("INSERT INTO {} VALUES ({})" \
+                  .format(dbname, ', '.join(['\'{}\''.format(row[var]) if type(row[var]) is str 
+                                             else '{}'.format(row[var]) for var in df_transfer.columns])))
+
 conn.commit()
 conn.close()
-
