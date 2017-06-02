ce284ccce8e58dbdbada74276a4680f2e92a5a36
Adam Anderson <adama@fnal.gov>
2017-06-02 10:56:15 -0500

    Update to database management to handle new condor submission

--- build_database.py
+++ build_database.py
@@ -6,6 +6,7 @@ import numpy as np
 from spt3g import core, cluster
 import argparse as ap
 import datetime
+import tarfile
 
 P0 = ap.ArgumentParser(description='Tool for building SPT3G data quality and '
                        'transfer databases.', formatter_class=ap.RawTextHelpFormatter)
@@ -79,7 +80,7 @@ if args.mode == 'rebuild':
     conn.commit()
     conn.close()
 
-n_files_per_worker = 20
+n_files_per_worker = 100
 
 conn = sqlite3.connect(dbfile)
 conn.row_factory = sqlite3.Row
@@ -108,14 +109,16 @@ for index, row in df_transfer.iterrows():
             # get start time by skimming files
             first_file = path + '0000.g3'
             try:
-                f = core.G3File(first_file)
+                # f = core.G3File(first_file)
                 print('Processing {}'.format(first_file))
-                frame = f.next()
-                start_time = frame['ObservationStart'].time
-                dt = datetime.datetime.fromtimestamp(
-                    int(start_time/core.G3Units.second))
-                date = dt.strftime('%d/%m/%Y')
-                time = dt.strftime('%H:%M')
+                # frame = f.next()
+                # start_time = frame['ObservationStart'].time
+                # dt = datetime.datetime.fromtimestamp(
+                #     int(start_time/core.G3Units.second))
+                # date = dt.strftime('%d/%m/%Y')
+                # time = dt.strftime('%H:%M')
+                date = 0
+                time = 0
             except Exception as e:
                 print('ERROR processing {}: {}'.format(first_file, e))
                 path = 'none'
@@ -162,7 +165,9 @@ result = c.execute('SELECT * FROM {} WHERE plots_complete == 0'.format(dbname)).
 plot_queue = [] # list of observations to process
 for row in result:
     if row['plots_inprogress'] == 0 and row['autoproc_path'] != 'none':
-        plot_queue.append(row['autoproc_path'])
+        plot_queue.append((os.path.dirname(row['autoproc_path']).split('/')[-1],
+                           row['autoproc_path'],
+                           '{}/nominal_online_cal.g3'.format(row['path'])))
         c.execute('UPDATE {} SET {} WHERE observation == {}'.format(
                     dbname, ('plots_inprogress = 1'), row['observation']))
 
@@ -173,16 +178,33 @@ jbatch = 0
 timestamp = '{:%Y%m%d_%H%M%S}'.format(datetime.datetime.now())
 while jbatch*n_files_per_worker < len(plot_queue):
     # write the pathlist file
-    n_to_submit = np.min([n_files_per_worker, len(plot_queue)])
+    startfile = jbatch*n_files_per_worker
+    stopfile = np.min([(jbatch+1)*n_files_per_worker, 
+                       len(plot_queue)])
     pathlist_file = '{}_{}.txt'.format(timestamp, jbatch)
-    output_file = '{}_{}.tar.gz'.format(timestamp, jbatch)
-    with open(pathlist_file, 'w') as f:
-        for p in plot_queue[:n_to_submit]:
-            f.write(p+'\n')
+    output_file = '{}_{}_plots.tar.gz'.format(timestamp, jbatch)
 
+    # tar up relevant g3 files
+    tar_input_fname = '{}_{}_input_data.tar'.format(timestamp, jbatch)
+    tar_cal_fname = '{}_{}_online_cal.tar'.format(timestamp, jbatch)
+    tar_input = tarfile.open(tar_input_fname, 'w')
+    tar_cal = tarfile.open(tar_cal_fname, 'w')
+    for p in plot_queue[startfile:stopfile]:
+        tar_input.add(p[1])
+        tar_cal.add(p[2])
+    tar_input.close()
+    tar_cal.close()
+
+    input_files = []
+    with open(pathlist_file, 'w') as f:
+        for p in plot_queue[startfile:stopfile]:
+            f.write('\t'.join(p) + '\n')
+            input_files.append(p[1])
+            input_files.append(p[2])
+            
     condor_configs = {
         'script': 'plotter.py',
-        'args': ['{}'.format(pathlist_file)],
+        'args': ['{}'.format(pathlist_file), 'plots'],
         'log_root': args.logdir,
         'request_cpus': 1,
         'request_memory': 2*core.G3Units.GB,
@@ -190,10 +212,14 @@ while jbatch*n_files_per_worker < len(plot_queue):
         'requirements': '(OpSysAndVer == "CentOS6" || OpSysAndVer == "RedHat6" || OpSysAndVer == "SL6")',
         'grid_proxy': '{}'.format(args.proxycert),
         'when_to_transfer_output': 'ON_EXIT',
-        'aux_input_files': ['{}'.format(pathlist_file)],
-        'input_files': plot_queue[(jbatch*n_files_per_worker):
-                                  (jbatch*np.max([n_files_per_worker, len(plot_queue)]))],
+        'aux_input_files': [pathlist_file],
+        'input_files': [tar_input_fname, tar_cal_fname],
         'output_root': args.plotdir,
-        'output_files': [output_file]
+        'output_files': [output_file],
+        'globus_uri': 'gsiftp://gridftp.grid.uchicago.edu:2811/cephfs',
+        'user_code': '\n'.join(['tar -xvf {}'.format(tar_input_fname),
+                                'tar -xvf {}'.format(tar_cal_fname)])
     }
+    print tar_input_fname
     cluster.condor_submit(**condor_configs)
+    jbatch = jbatch+1
