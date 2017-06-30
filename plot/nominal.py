ca517e87b77c3759f4055436517469e25a31134a
Cory Cotter <ccotter@fnal.gov>
2017-06-30 10:15:01 -0500

    Changed plotting method so that it is simple to add new plot types.

---
+++ plot/nominal.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from spt3g import core, calibration
+
+# makes a plot of the nominal offset given the date
+def nominal_plot(request):
+    data = [frame for frame in core.G3File(request['path'] + 'nominal_online_cal.g3')]
+
+    x = [item.x_offset for key, item in data[0]['NominalBolometerProperties'].items()]
+    y = [item.y_offset for key, item in data[0]['NominalBolometerProperties'].items()]
+
+
+    plt.scatter(x, y)
+    plt.xlabel('x Offset')
+    plt.ylabel('y Offset')
+    plt.title('Nominal Bolometer Positions of ' + request['source'] + ' at time ' + request['observation'])
+    plt.savefig('plot.png')
+    plt.close()
+
+
---
+++ plot/offline.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from spt3g import core, calibration
+
+# makes a plot of the offline offset given the date
+def offline_plot(request):
+    data = [frame for frame in core.G3File('/spt/user/production/calibration/' + request['source'] + '/' + request['observation'] + '.g3')]
+
+    x = []
+    y = []
+    for key, item in data[0]['PointingOffsetX'].items():
+        x_off = data[0]['PointingOffsetX'][key]
+        y_off = data[0]['PointingOffsetY'][key]
+        if (x_off < 0.02 and x_off > -0.02 and y_off > -0.02 and y_off < 0.02):
+            x.append(x_off)
+            y.append(y_off)
+
+
+    plt.scatter(x, y)
+    plt.xlabel('x Offset')
+    plt.ylabel('y Offset')
+    plt.title('Offline Bolometer Positions of ' + request['source'] + ' at time ' + request['observation'])
+    plt.savefig('plot.png')
+    plt.close()
+
+
