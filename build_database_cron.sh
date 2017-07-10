18c0ea8c21c17b67329ab2ad4673e7097ba61ade
Cory Cotter <ccotter@fnal.gov>
2017-07-10 11:46:30 -0500

    Removed old files and updated required packages. Also included config file.

--- build_database_cron.sh
+++
@@ -1,3 +0,0 @@
-eval `/cvmfs/spt.opensciencegrid.org/py2-v1/setup.sh`
-alias 3g=/home/adama/SPT/spt3g_software/build/env-shell.sh
-3g python /scratch/arahlin/public_html/spt_dq_system/build_database.py update 1> /scratch/arahlin/public_html/spt_dq_system/cron.out
