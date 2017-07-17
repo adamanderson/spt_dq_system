78b80e8a635485e4a2d7284e5e8b81b2f7b36b0a
Cory Cotter <cjcotter2@wisc.edu>
2017-07-17 10:58:13 -0500

    fixed plot_type error in main plotting file during conversion to multiple plotting modes.

--- plot/_plot.py
+++ plot/_plot.py
@@ -25,19 +25,19 @@ def plot(request):
     # remove cached plots if plotting file has changed
     if (path.isfile(plot_file)):
       plot_time = stat(plot_file).st_mtime
-      file_time = stat('./plot/' + plot_type + '.py').st_mtime
+      file_time = stat('./plot/' + request['plot_type'] + '.py').st_mtime
       if (plot_time > file_time):
         return
     # load python file and function
     # only import from plot package so that the user passed plot_type
     # cannot import arbitrary modules
-    module = import_module('plot.' + plot_type)
+    module = import_module('plot.' + request['plot_type'])
   except Exception as e:
     err_handler(e, request)
   try:
-    func = getattr(module, plot_type)
+    func = getattr(module, request['plot_type'])
   except AttributeError as e:
-    err_handler(e, request, 'Error. Plotting file for ' + plot_type
+    err_handler(e, request, 'Error. Plotting file for ' + request['plot_type']
         + ''' formatted incorrectly. There needs to be a
         function with the same name as the file.''')
   # call plotting function
@@ -47,7 +47,7 @@ def plot(request):
     # special case for key error because it likely means a key
     # doesn't exist in a data file.
     err_handler(e, request, 'Error making plot ' + request['source'] +
-        ' ' + requset['observation'] + ' ' + plot_type
+        ' ' + requset['observation'] + ' ' + request['plot_type']
         + '''. Likely that requested data doesn't exist in the data file.
         Check the log for more details.''')
   except Exception as e:
@@ -57,15 +57,15 @@ def plot(request):
   if (isinstance(plot, str)):
     err_handler(Exception(plot), request,
         'Error making plot ' + request['source'] + ' ' +
-        request['observation'] + ' ' + plot_type + '. ' + plot);
+        request['observation'] + ' ' + request['plot_type'] + '. ' + plot);
 
   # handle if plot is not a figure
   if (isinstance(plot, plt.Figure)):
     plot.savefig(plot_file, dpi=200)
   else:
-    err_handler(TypeError('Return object from plot function ' + plot_type
+    err_handler(TypeError('Return object from plot function ' + request['plot_type']
         + ' is not a matplotlib figure.'), request, 'Error making plot '
-        + plot_type
+        + request['plot_type']
         + '. Plot file formatted incorrectly. Check the log for details.')
 
 def individual():
@@ -73,12 +73,12 @@ def individual():
   request = {'source': sys.argv[2], 'observation': sys.argv[3]}
 
   for plot_type in sys.argv[4:]:
-      request['type'] = plot_type
+      request['plot_type'] = plot_type
       plot(request);
 
 def timeseries():
   # load arguments into a dict to be passed to plotting functions
-  request = {'source': sys.argv[2], 'type': sys.argv[3], 'observation': ' '.join(sys.argv[4:])}
+  request = {'source': sys.argv[2], 'plot_type': sys.argv[3], 'observation': ' '.join(sys.argv[4:])}
   plot(request);
 
 #start process
