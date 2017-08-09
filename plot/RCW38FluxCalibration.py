cd0ad37a21cb7f717c5eff82cde6b79788a46e63
Adam Anderson <adama@fnal.gov>
2017-08-09 17:29:55 -0500

    Split RCW38 plots by band.

--- plot/RCW38FluxCalibration.py
+++ plot/RCW38FluxCalibration.py
@@ -6,27 +6,34 @@ from spt3g import core, calibration
 # makes a plot of the offline offset given the date
 def RCW38FluxCalibration(request):
   try:
-    data = [frame for frame in core.G3File('/spt/user/production/calibration/' + request['source'] + '/' + request['observation'] + '.g3')]
+    data = [fr for fr in core.G3File('/spt/user/production/calibration/{}/{}.g3' \
+                                       .format(request['source'], request['observation']))]
+    boloprops = [fr for fr in core.G3File('/spt/data/bolodata/downsampled/{}/{}/nominal_online_cal.g3' \
+                                            .format(request['source'], request['observation']))] \
+                                            [0]["NominalBolometerProperties"]
   except RuntimeError:
     return "Could not find data file."
 
-  x = []
+  bands = [90, 150, 220]
   try:
-    for key, item in data[0]['RCW38FluxCalibration'].items():
-      if (math.isnan(item)):
-        continue
-      x.append(item)
+    cal_dict = {}
+    for band in bands:
+      cal_dict[band] = np.array([data[0]['RCW38FluxCalibration'][bolo] \
+                          for bolo in data[0]['RCW38FluxCalibration'].keys() \
+                          if boloprops[bolo].band / core.G3Units.GHz == band])
   except KeyError:
-    return "RCW38 flux calibration data does not exist for this observation."
+    return "RCW38 flux calibration does not exist for this observation."
 
-  std = np.std(x)
-  mean = np.mean(x)
-  y = np.asarray(x)
-  for i in range(len(y)):
-    if (y[i] > mean + 2.5 * std or y[i] < mean - 2.5 * std):
-      x.remove(y[i])
   fig = plt.figure()
-  n, bins, patches = plt.hist(x, bins=40)
+  for band in bands:
+    x = np.array(cal_dict[band])
+    x = x[np.isfinite(x)]
+
+    plt.hist(x,
+             bins=np.linspace(-90, 10, 50),
+             label='{} GHz'.format(band),
+             histtype='step')
+  plt.legend(loc='upper left')
 
   plt.xlabel('RCW38FluxCalibration')
   plt.title('RCW38FluxCalibration of ' + request['source'] + ' at time ' + request['observation'])
--- plot/RCW38IntegralFlux.py
+++ plot/RCW38IntegralFlux.py
@@ -6,27 +6,34 @@ from spt3g import core, calibration
 # makes a plot of the offline offset given the date
 def RCW38IntegralFlux(request):
   try:
-    data = [frame for frame in core.G3File('/spt/user/production/calibration/' + request['source'] + '/' + request['observation'] + '.g3')]
+    data = [fr for fr in core.G3File('/spt/user/production/calibration/{}/{}.g3' \
+                                       .format(request['source'], request['observation']))]
+    boloprops = [fr for fr in core.G3File('/spt/data/bolodata/downsampled/{}/{}/nominal_online_cal.g3' \
+                                            .format(request['source'], request['observation']))] \
+                                            [0]["NominalBolometerProperties"]
   except RuntimeError:
     return "Could not find data file."
 
-  x = []
+  bands = [90, 150, 220]
   try:
-    for key, item in data[0]['RCW38IntegralFlux'].items():
-      if (math.isnan(item)):
-        continue
-      x.append(item)
+    cal_dict = {}
+    for band in bands:
+      cal_dict[band] = np.array([data[0]['RCW38IntegralFlux'][bolo] \
+                          for bolo in data[0]['RCW38IntegralFlux'].keys() \
+                          if boloprops[bolo].band / core.G3Units.GHz == band])
   except KeyError:
-    return "RCW38 integral flux data does not exist for this observation."
+    return "RCW38 integral flux does not exist for this observation."
 
-  std = np.std(x)
-  mean = np.mean(x)
-  y = np.asarray(x)
-  for i in range(len(y)):
-    if (y[i] > mean + 2.5 * std or y[i] < mean - 2.5 * std):
-      x.remove(y[i])
   fig = plt.figure()
-  n, bins, patches = plt.hist(x, bins=40)
+  for band in bands:
+    x = np.array(cal_dict[band])
+    x = x[np.isfinite(x)]
+
+    plt.hist(x,
+             bins=np.linspace(2e-7, 6e-7, 50),
+             label='{} GHz'.format(band),
+             histtype='step')
+  plt.legend(loc='upper left')
 
   plt.xlabel('RCW38IntegralFlux')
   plt.title('RCW38IntegralFlux of ' + request['source'] + ' at time ' + request['observation'])
