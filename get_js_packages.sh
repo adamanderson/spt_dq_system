51f490281065d5813eabc7f111024164f8b523ee
Adam Anderson <adama@fnal.gov>
2017-12-21 23:34:59 +1300

    Add script to wget js dependencies. Can't use googleapis at pole!!

---
+++ get_js_packages.sh
@@ -0,0 +1,9 @@
+cd js
+wget https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js
+wget https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
+wget https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+wget https://code.jquery.com/jquery-1.12.4.js
+wget https://code.jquery.com/ui/1.12.1/jquery-ui.js
+wget https://momentjs.com/downloads/moment.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/css/tabulator.min.css
+wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/js/tabulator.min.js
