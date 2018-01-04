1fda8a4d7eb3f46af2df71533408c3cdeda16d6b
Adam Anderson <adama@fnal.gov>
2018-01-04 13:32:58 +1300

    Switch to a few more local versions of dependencies.

--- get_js_packages.sh
+++ get_js_packages.sh
@@ -6,6 +6,7 @@ wget https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 wget https://code.jquery.com/jquery-1.12.4.js
 wget https://code.jquery.com/ui/1.12.1/jquery-ui.js
 wget https://momentjs.com/downloads/moment.min.js
-wget https://github.com/olifolkerd/tabulator/archive/master.zip
-unzip master.zip
-rm master.zip
+wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/css/tabulator.min.css
+mv tabulator.min.css ../css
+wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/js/tabulator.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js
