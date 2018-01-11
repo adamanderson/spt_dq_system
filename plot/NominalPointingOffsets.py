b8d53d4b3c27338440883a8507f3b26e3c4b1c39
Adam Anderson <adama@fnal.gov>
2018-01-11 14:50:11 +1300

    Update and rename nominal and offline pointing offset plots to suck less. Also don't try to plot offline pointing for all observation types.

---
+++ plot/NominalPointingOffsets.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from spt3g import core, calibration
+
+# makes a plot of the nominal offset given the date
+def NominalPointingOffsets(request):
+  try:
+    data = [fr for fr in core.G3File('{}/downsampled/{}/{}/nominal_online_cal.g3' \
+                                                  .format(request['bolodatapath'],
+                                                          request['source'],
+                                                          request['observation']))] \
+                                                  [0]["NominalBolometerProperties"]
+  except RuntimeError:
+    return "Could not find data file."
+
+  x = []
+  y = []
+  for key, item in data.items():
+    x_off = item.x_offset
+    y_off = item.y_offset
+    if (x_off < 0.02 and x_off > -0.02 and y_off > -0.02 and y_off < 0.02):
+      x.append(x_off / core.G3Units.deg)
+      y.append(y_off / core.G3Units.deg)
+
+
+  fig = plt.figure()
+  plt.scatter(x, y, s=10)
+  plt.xlabel('x Offset [deg]')
+  plt.ylabel('y Offset [deg]')
+  plt.title('Nominal Bolometer Positions of ' + request['source'] + ' at time ' + request['observation'])
+  plt.axis(np.array([-0.02, 0.02, -0.02, 0.02]) / core.G3Units.deg)
+  return fig
--- plot/OfflinePointingOffsets.py
+++ plot/OfflinePointingOffsets.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 from spt3g import core, calibration
 
 # makes a plot of the offline offset given the date
-def offline(request):
+def OfflinePointingOffsets(request):
   try:
-    data = [frame for frame in core.G3File('/spt/user/production/calibration/' + request['source'] + '/' + request['observation'] + '.g3')]
+    data = [fr for fr in core.G3File('{}/calibration/{}/{}.g3' \
+                                             .format(request['caldatapath'],
+                                                     request['source'],
+                                                     request['observation']))]
   except RuntimeError:
     return "Could not find data file."
 
@@ -16,15 +19,16 @@ def offline(request):
       x_off = data[0]['PointingOffsetX'][key]
       y_off = data[0]['PointingOffsetY'][key]
       if (x_off < 0.02 and x_off > -0.02 and y_off > -0.02 and y_off < 0.02):
-        x.append(x_off)
-        y.append(y_off)
+        x.append(x_off / core.G3Units.deg)
+        y.append(y_off / core.G3Units.deg)
   except KeyError:
     return "Offline calibration does not exist for this observation."
 
 
   fig = plt.figure()
-  plt.scatter(x, y)
+  plt.scatter(x, y, s=10)
   plt.xlabel('x Offset')
   plt.ylabel('y Offset')
   plt.title('Offline Bolometer Positions of ' + request['source'] + ' at time ' + request['observation'])
+  plt.axis(np.array([-0.02, 0.02, -0.02, 0.02]) / core.G3Units.deg)
   return fig
