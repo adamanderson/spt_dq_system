d206df09836ebf5da25810eace9e9fe64c444d8f
Adam Anderson <adama@fnal.gov>
2018-01-14 01:53:39 +1300

    Placeholder that does a transaction with the server and adds a plot from the last observation to a basic HTML table.

--- js/summary.js
+++ js/summary.js
@@ -2,4 +2,61 @@ function open_table(table) {
     $('#recent').hide();
     $('#lastobs').hide();
     $('#'+table).show();
+
+    if(table == "lastobs")
+	load_latestobs();
+}
+
+// create sse listener
+var es = new EventSource("/sse");
+
+function load_latestobs() {
+    // toy request for demonstration purposes
+    datareq = [{observation: '32537722',
+		source: 'calibrator',
+		table: 'transfer',
+		plot_type: 'CalHistogram',
+		func:'individual'}];
+    
+    var items = [];
+    var nTotalPlots = 0;
+    var sseid;
+
+    // gets the IDs for each plot to be produced and keeps track of the number
+    // that have been created
+    $.get("sseid", function(id, status) {
+	    sseid = id;
+	    // counts the images that have finished being created         
+	    var plot_ctr = 0;
+
+	    es.addEventListener('out' + sseid, function (event) {
+		    if (event.data.split('fln').length == 2) {
+			path =  event.data.split('fln')[1].slice(0,-1);
+			items.push({src: 'img/' + path, w: 0, h: 0});
+		    }
+		    else if (event.data.slice(1, 4) == 'plt') {
+			plot_ctr++;
+			
+			if (nTotalPlots == plot_ctr) {
+			    $.each(items, function(i, img) {
+				    console.log(img);
+				    $('#calSN').attr('src', img.src);
+				});
+			}
+		    }
+		});
+	    es.addEventListener('err' + sseid, function (event) {
+		    display_win.show_err(event.data);
+		});
+
+	    // loop over each observation
+	    $.each(datareq, function(i, obsdata) {
+		    obsdata['sseid'] = sseid;
+		    
+		    // request the plots
+		    $.get("data_req", obsdata, function(data, status) {
+			    nTotalPlots += datareq.length;
+			});
+		});
+	});
 }
