5530f241ef8d97d2d8e26ff4eac6505b104ad53b
Adam Anderson <adama@fnal.gov>
2018-02-05 22:15:26 -0600

    Remove redundant minified versions of jquery and jquery-ui. For reasons that currently confuse me, the minified versions of these package do not current work.

--- get_js_packages.sh
+++ get_js_packages.sh
@@ -1,8 +1,6 @@
 cd js
-wget https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js
 wget https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
 mv jquery-ui.css ../css
-wget https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 wget https://code.jquery.com/jquery-1.12.4.js
 wget https://code.jquery.com/ui/1.12.1/jquery-ui.js
 wget https://momentjs.com/downloads/moment.min.js
