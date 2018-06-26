570f951d5c723a20850f2876e5558a48de2ff72f
dyang2018 <dyang@g.hmc.edu>
2018-06-25 20:56:24 -0700

    organized hard coded directories

--- condor/individual.py
+++ condor/individual.py
@@ -4,28 +4,38 @@ from spt3g import core, std_processing, mapmaker, dfmux, calibration, xtalk, tod
 import argparse as ap
 import time
 
-''' 
-submitting plots for all the individual observation plots, some hard coding with directories, especially with
-the tar files of calibration data and the script location
+'''
+submitting plots for all the individual observation plots. This submits condor jobs
 
 
 '''
-plotpath='/scratch/dyang18/dq_plots/'
+
+
 P = ap.ArgumentParser(description='making individual observation plots on condor',
               formatter_class=ap.ArgumentDefaultsHelpFormatter)
 
 
-P.add_argument('source', action='store', 
+P.add_argument('source', action='store',
            default=None, help='name of source')
 P.add_argument('year',action='store', default=None,
            help='the year to gather data for')
 
 args = P.parse_args()
+
 source=args.source
 year=args.year
 
-datatar = tarfile.open('/spt/user/dyang18/tar/'+str(year)+'/'+str(year)+'_'+source+'.tar.gz')
-bolotar=  tarfile.open('/spt/user/dyang18/tar/'+str(year)+'/'+str(year)+'_'+source+'BolometerProperties.tar.gz')
+#directories to be modified
+plotpath='/scratch/dyang18/dq_plots/'
+datatarpath='/spt/user/dyang18/tar/'+str(year)+'/'+str(year)+'_'+source+'.tar.gz'
+bolotarpath='/spt/user/dyang18/tar/'+str(year)+'/'+str(year)+'_'+source+'BolometerProperties.tar.gz'
+script='/home/dyang18/spt3g/spt3g_software/scratch/dyang18/'+source+'PlotCondor.py'
+log_root='/scratch/dyang18'
+grid_proxy='/home/dyang18/dyang'
+outputpath='/spt/user/dyang18/output/'
+
+datatar = tarfile.open(datatarpath)
+bolotar=  tarfile.open(bolotarpath)
 
 dat=[member.name for member in datatar.getmembers()]
 bolo=[member.name for member in bolotar.getmembers()]
@@ -44,27 +54,24 @@ submit = [x for x in adata if x not in aplots]
 #section for developing the date range to make plots for
 
 def split(x,n):
-    return [x[i:i+n] for i in range(0, len(x), n)]  
+    return [x[i:i+n] for i in range(0, len(x), n)]
 # splitting up the list of files and then submit them
 
 final=split(submit, 50)
 
 #Submission section for individual
-script='/home/dyang18/spt3g/spt3g_software/scratch/dyang18/'+source+'PlotCondor.py'
 
-log_root='/scratch/dyang18'
 
-input_files=['/spt/user/dyang18/tar/'+str(year)+'/'+str(year)+'_'+source+'.tar.gz','/spt/user/dyang18/tar/'+str(year)+'/'+str(year)+'_'+source+'BolometerProperties.tar.gz']
+input_files=[datatarpath,bolotarpath]
 output_files=[source+".tar.gz"]
-#bundle these things together in order to transfer one giant thing
-grid_proxy='/home/dyang18/dyang'
+
 
 for i in range(len(final)):
     f=final[i]
-    
+
     args=[ '-o' +' ' +' '.join(f)]
     jobname='condor_{}_batch_{}'.format(time.strftime('%Y%m%d_%H%M%S'),str(i))
-    output_root='/spt/user/dyang18/output/'+jobname
+    output_root=outputpath+jobname
     i+=1
     cluster.condor_submit(script=script, args=args, caller='python', jobname=jobname,
                       log_root=log_root, output_root=output_root, input_files=input_files,
@@ -75,6 +82,3 @@ for i in range(len(final)):
                       request_disk=6*core.G3Units.GB, queue=1,
                       when_to_transfer_output='ON_EXIT', create_only=False,
                       globus_uri='gsiftp://gridftp.grid.uchicago.edu:2811/cephfs')
-
-
-
