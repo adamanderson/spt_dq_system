f162255dfcd10ee574bdd0f0f0068489d5d6bd44
Adam Anderson <adama@fnal.gov>
2018-12-28 23:40:07 +1300

    Set up python3 environment for building `spt3g_software` in setup script.

--- setup.sh
+++ setup.sh
@@ -22,13 +22,15 @@ wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/js/tabulator.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js
 cd ..
 
-# clone spt3g_software
+# clone spt3g_software and set up python3 environment
 if [ "$1" == "pole" ];
 then
 	git clone sptviewer@anal:sptRepos/spt3g_software
+	eval `/software/clustertools/py3-v1/setup.sh`
 elif [ "$1" == "north" ];
 then
 	git clone https://github.com/SouthPoleTelescope/spt3g_software.git
+	eval `/cvmfs/spt.opensciencegrid.org/py3-v1/RHEL_7_x86_64/setup.sh`
 else
 	echo "Did not specify site argument of 'pole' or 'north'. "\
 	"Cannot clone spt3g_software."
