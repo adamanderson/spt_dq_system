1cbe1fdf2fbeea34220a20cf38aa22d0e800926e
Adam Anderson <adama@fnal.gov>
2018-12-28 17:54:31 +1300

    Remake nominal bolo offsets plot.

--- plot/NominalPointingOffsets.py
+++ plot/NominalPointingOffsets.py
@@ -15,19 +15,39 @@ def NominalPointingOffsets(request):
 
   x = []
   y = []
+  bands = []
+  pols = []
   for key, item in data.items():
     x_off = item.x_offset
     y_off = item.y_offset
     if (x_off < 0.02 and x_off > -0.02 and y_off > -0.02 and y_off < 0.02):
       x.append(x_off / core.G3Units.deg)
       y.append(y_off / core.G3Units.deg)
+      bands.append(item.band / core.G3Units.GHz)
+      pols.append(item.physical_name.split('.')[-1])
+  x = np.array(x)
+  y = np.array(y)
+  bands = np.array(bands)
+  pols = np.array(pols)
 
-
-  fig = plt.figure()
-  plt.scatter(x, y, s=10)
+  fig = plt.figure(figsize=(15,5))
+  for jband, band in enumerate([90, 150, 220]):
+    plt.subplot(1,3,jband+1)
+    plt.scatter(x[(bands==band) & (pols=='x')],
+                y[(bands==band) & (pols=='x')],
+                s=10, marker='_', linewidth=1, color='b', label='x pol')
+    plt.scatter(x[(bands==band) & (pols=='y')],
+                y[(bands==band) & (pols=='y')],
+                s=10, marker='|', linewidth=1, color='r', label='y pol')
+    plt.axis(np.array([-0.02, 0.02, -0.02, 0.02]) / core.G3Units.deg)
+    plt.grid()
+    plt.legend(prop={'size':10})
+    plt.title('{} GHz'.format(band))
+    
+  plt.subplot(1,3,2)
   plt.xlabel('x Offset [deg]')
+  plt.subplot(1,3,1)
   plt.ylabel('y Offset [deg]')
-  plt.title('Nominal Bolometer Positions of ' + request['source'] + ' at time ' + request['observation'])
-  plt.axis(np.array([-0.02, 0.02, -0.02, 0.02]) / core.G3Units.deg)
-  plt.grid()
+  plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+  plt.suptitle('{}: nominal bolometer offsets'.format( request['observation']))
   return fig
