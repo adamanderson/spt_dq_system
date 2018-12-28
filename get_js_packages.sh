78b7783cb2ef04cb62f09acad31e53e992d5d0ee
Adam Anderson <adama@fnal.gov>
2018-12-28 23:32:01 +1300

    Clone and compile spt3g_software in setup script.

--- get_js_packages.sh
+++ get_js_packages.sh
@@ -1,3 +1,15 @@
+# check arguments
+if [ "$1" != "pole" ] && [ "$2" != "north" ];
+then
+	echo "Did not specify site argument of 'pole' or 'north'. "\
+	"Refusing to run."
+	exit 1
+fi
+
+# get node.js dependencies (server-side)
+npm install
+
+# get local copy of js dependencies (client-side)
 cd js
 wget https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
 mv jquery-ui.css ../css
@@ -8,3 +20,24 @@ wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/css/tabulator.min.cs
 mv tabulator.min.css ../css
 wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/js/tabulator.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js
+cd ..
+
+# clone spt3g_software
+if [ "$1" == "pole" ];
+then
+	git clone sptviewer@anal:sptRepos/spt3g_software
+elif [ "$1" == "north" ];
+then
+	git clone https://github.com/SouthPoleTelescope/spt3g_software.git
+else
+	echo "Did not specify site argument of 'pole' or 'north'. "\
+	"Cannot clone spt3g_software."
+	exit 1
+fi
+
+# build spt3g_software
+mkdir spt3g_software/build
+cd spt3g_software/build
+cmake ..
+make -j4
+cd ..
