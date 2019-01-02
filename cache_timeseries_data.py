c587036926920597a0967192d97d158fab056cab
Adam Anderson <adama@fnal.gov>
2019-01-03 00:26:35 +1300

    Add MAT5A plots to summary plotter.

--- cache_timeseries_data.py
+++ cache_timeseries_data.py
@@ -322,10 +322,47 @@ def rcw38_sky_transmission(frame, selector_dict):
 
     return data_on_selection
 
+def median_mat5a_fluxcal(frame, selector_dict):
+    if 'MAT5AFluxCalibration' not in frame.keys():
+        return None
+    return compute_median(frame, 'MAT5AFluxCalibration', selector_dict)
+
+def median_mat5a_intflux(frame, selector_dict):
+    if 'MAT5AIntegralFlux' not in frame.keys():
+        return None
+    return compute_median(frame, 'MAT5AIntegralFlux', selector_dict)
+
+def mat5a_sky_transmission(frame, selector_dict):
+    if 'MAT5ASkyTransmission' not in frame.keys():
+        return None
+
+    data_on_selection = {}
+
+    for select_values, f_select_list in selector_dict.items():
+        # prep the dictionary for output
+        for keylist in [select_values[:j] for j in np.arange(len(select_values))+1]:
+            try:
+                reduce(operator.getitem, keylist, data_on_selection)
+            except:
+                reduce(operator.getitem, keylist[:-1], data_on_selection)[keylist[-1]] = {}
+
+        # get data that satisfies the selection and compute median
+        if str(select_values[-1]) in frame['MAT5ASkyTransmission'].keys():
+            reduce(operator.getitem, select_values[:-1], data_on_selection)[select_values[-1]] = \
+                frame['MAT5ASkyTransmission'][str(select_values[-1])]
+        else:
+            reduce(operator.getitem, select_values[:-1], data_on_selection)[select_values[-1]] = np.nan
+
+    return data_on_selection
+
+
     
 function_dict = {'RCW38':             {'RCW38SkyTransmission': rcw38_sky_transmission},
                  'RCW38-pixelraster': {'MedianRCW38FluxCalibration': median_rcw38_fluxcal,
                                        'MedianRCW38IntegralFlux': median_rcw38_intflux},
+                 'MAT5A':             {'MAT5ASkyTransmission': mat5a_sky_transmission},
+                 'MAT5A-pixelraster': {'MedianMAT5AFluxCalibration': median_mat5a_fluxcal,
+                                       'MedianMAT5AIntegralFlux': median_mat5a_intflux},
                  'calibrator':        {'MedianCalSN_4Hz': median_cal_sn_4Hz,
                                        'MedianCalResponse_4Hz': median_cal_response_4Hz,
                                        'AliveBolosCal_4Hz': alive_bolos_cal_4Hz},
@@ -652,10 +689,12 @@ for mindate, maxdate in zip(date_boundaries[:-1], date_boundaries[1:]):
             is_empty = True
             for band in [90, 150, 220]:
                 median_rcw38 = np.array([data['RCW38-pixelraster'][obsid]['MedianRCW38FluxCalibration'][wafer][band]
-                                for obsid in data['RCW38-pixelraster']])
+                                         for obsid in obsids
+                                         if 'MedianRCW38FluxCalibration' in data['RCW38-pixelraster'][obsid]])
 
                 timestamps = [obsid_to_g3time(int(obsid)).time / core.G3Units.seconds \
-                              for obsid in obsids]
+                              for obsid in obsids
+                              if 'MedianRCW38FluxCalibration' in data['RCW38-pixelraster'][obsid]]
                 dts = np.array([datetime.datetime.fromtimestamp(ts) for ts in timestamps])
                 datenums = mdates.date2num(dts)
 
@@ -687,10 +726,12 @@ for mindate, maxdate in zip(date_boundaries[:-1], date_boundaries[1:]):
             is_empty = True
             for band in [90, 150, 220]:
                 median_rcw38 = np.array([data['RCW38-pixelraster'][obsid]['MedianRCW38IntegralFlux'][wafer][band]
-                                for obsid in data['RCW38-pixelraster']])
+                                         for obsid in obsids
+                                         if 'MedianRCW38IntegralFlux' in data['RCW38-pixelraster'][obsid]])
 
                 timestamps = [obsid_to_g3time(int(obsid)).time / core.G3Units.seconds \
-                              for obsid in obsids]
+                              for obsid in obsids
+                              if 'MedianRCW38IntegralFlux' in data['RCW38-pixelraster'][obsid]]
                 dts = np.array([datetime.datetime.fromtimestamp(ts) for ts in timestamps])
                 datenums = mdates.date2num(dts)
 
@@ -723,10 +764,10 @@ for mindate, maxdate in zip(date_boundaries[:-1], date_boundaries[1:]):
             for band in [90, 150, 220]:
                 rcw38_skytrans = np.array([data['RCW38'][obsid]['RCW38SkyTransmission'][wafer][band]
                                            for obsid in obsids
-                                           if 'RCW38SkyTransmission' in data['RCW38'][obsid].keys()])
+                                           if 'RCW38SkyTransmission' in data['RCW38'][obsid]])
                 timestamps = [obsid_to_g3time(int(obsid)).time / core.G3Units.seconds
                               for obsid in obsids
-                              if 'RCW38SkyTransmission' in data['RCW38'][obsid].keys()]
+                              if 'RCW38SkyTransmission' in data['RCW38'][obsid]]
                 dts = np.array([datetime.datetime.fromtimestamp(ts) for ts in timestamps])
                 datenums = mdates.date2num(dts)
 
@@ -748,6 +789,116 @@ for mindate, maxdate in zip(date_boundaries[:-1], date_boundaries[1:]):
             plt.title('RCW38 Sky Transmission ({})'.format(wafer))
             plt.tight_layout()
             plt.savefig('{}/rcw38_sky_transmission_{}.png'.format(outdir, wafer))
+            plt.close()
+
+    def plot_median_mat5a_fluxcal(data):
+        for wafer in wafer_list:
+            obsids = [obsid for obsid in data['MAT5A-pixelraster']]
+            f = plt.figure(figsize=(8,6))
+
+            is_empty = True
+            for band in [90, 150, 220]:
+                median_mat5a = np.array([data['MAT5A-pixelraster'][obsid]['MedianMAT5AFluxCalibration'][wafer][band]
+                                         for obsid in obsids
+                                         if 'MedianMAT5AFluxCalibration' in data['MAT5A-pixelraster'][obsid]])
+
+                timestamps = [obsid_to_g3time(int(obsid)).time / core.G3Units.seconds \
+                              for obsid in obsids
+                              if 'MedianMAT5AFluxCalibration' in data['MAT5A-pixelraster'][obsid]]
+                dts = np.array([datetime.datetime.fromtimestamp(ts) for ts in timestamps])
+                datenums = mdates.date2num(dts)
+
+                plt.plot(datenums[np.isfinite(median_mat5a)],
+                         median_mat5a[np.isfinite(median_mat5a)],
+                         'o', label='{} GHz'.format(band))
+
+                if len(median_mat5a[np.isfinite(median_mat5a)])>0:
+                    is_empty = False
+
+            if is_empty == False:
+                xfmt = mdates.DateFormatter('%m-%d %H:%M')
+                plt.gca().xaxis.set_major_formatter(xfmt)
+                plt.xticks(rotation=25)
+                plt.ylim([-100, 0])
+                plt.legend()
+            plt.xlabel('observation time')
+            plt.ylabel('median MAT5A flux calibration')
+            plt.title('MAT5A Flux Calibration ({})'.format(wafer))
+            plt.tight_layout()
+            plt.savefig('{}/median_mat5a_fluxcal_{}.png'.format(outdir, wafer))
+            plt.close()
+
+    def plot_median_mat5a_intflux(data):
+        for wafer in wafer_list:   
+            obsids = [obsid for obsid in data['MAT5A-pixelraster']]
+            f = plt.figure(figsize=(8,6))
+            
+            is_empty = True
+            for band in [90, 150, 220]:
+                median_mat5a = np.array([data['MAT5A-pixelraster'][obsid]['MedianMAT5AIntegralFlux'][wafer][band]
+                                         for obsid in obsids 
+                                         if 'MedianMAT5AIntegralFlux' in data['MAT5A-pixelraster'][obsid]])
+
+                timestamps = [obsid_to_g3time(int(obsid)).time / core.G3Units.seconds \
+                              for obsid in obsids
+                              if 'MedianMAT5AIntegralFlux' in data['MAT5A-pixelraster'][obsid]]
+                dts = np.array([datetime.datetime.fromtimestamp(ts) for ts in timestamps])
+                datenums = mdates.date2num(dts)
+
+                plt.plot(datenums[np.isfinite(median_mat5a)],
+                         median_mat5a[np.isfinite(median_mat5a)],
+                         'o', label='{} GHz'.format(band))
+
+                if len(median_mat5a[np.isfinite(median_mat5a)])>0:
+                    is_empty = False
+
+            if is_empty == False:
+                xfmt = mdates.DateFormatter('%m-%d %H:%M')
+                plt.gca().xaxis.set_major_formatter(xfmt)
+                plt.xticks(rotation=25)
+                plt.ylim([2e-7, 7e-7])
+                plt.legend()
+            plt.xlabel('observation time')
+            plt.ylabel('median MAT5A integral flux')
+            plt.title('MAT5A Integral Flux ({})'.format(wafer))
+            plt.tight_layout()
+            plt.savefig('{}/median_mat5a_intflux_{}.png'.format(outdir, wafer))
+            plt.close()
+
+    def plot_mat5a_sky_transmission(data):
+        for wafer in wafer_list:   
+            obsids = [obsid for obsid in data['MAT5A']]
+            f = plt.figure(figsize=(8,6))
+            
+            is_empty = True
+            for band in [90, 150, 220]:
+                mat5a_skytrans = np.array([data['MAT5A'][obsid]['MAT5ASkyTransmission'][wafer][band]
+                                           for obsid in obsids
+                                           if 'MAT5ASkyTransmission' in data['MAT5A'][obsid]])
+                timestamps = [obsid_to_g3time(int(obsid)).time / core.G3Units.seconds
+                              for obsid in obsids
+                              if 'MAT5ASkyTransmission' in data['MAT5A'][obsid]]
+                dts = np.array([datetime.datetime.fromtimestamp(ts) for ts in timestamps])
+                datenums = mdates.date2num(dts)
+
+                plt.plot(datenums[np.isfinite(mat5a_skytrans)],
+                         mat5a_skytrans[np.isfinite(mat5a_skytrans)],
+                         'o', label='{} GHz'.format(band))
+
+                if len(mat5a_skytrans[np.isfinite(mat5a_skytrans)])>0:
+                    is_empty = False
+
+            if is_empty == False:
+                xfmt = mdates.DateFormatter('%m-%d %H:%M')
+                plt.gca().xaxis.set_major_formatter(xfmt)
+                plt.xticks(rotation=25)
+                plt.ylim([0.85, 1.25])
+                plt.legend()
+            plt.xlabel('observation time')
+            plt.ylabel('MAT5A sky transmission')
+            plt.title('MAT5A Sky Transmission ({})'.format(wafer))
+            plt.tight_layout()
+            plt.savefig('{}/mat5a_sky_transmission_{}.png'.format(outdir, wafer))
             plt.close()
 
     def plot_median_noise(data, noise_type):
@@ -810,6 +961,9 @@ for mindate, maxdate in zip(date_boundaries[:-1], date_boundaries[1:]):
         plot_median_rcw38_fluxcal(data)
         plot_median_rcw38_intflux(data)
         plot_rcw38_sky_transmission(data)
+        plot_median_mat5a_fluxcal(data)
+        plot_median_mat5a_intflux(data)
+        plot_mat5a_sky_transmission(data)
         plot_median_noise(data, 'NEI_0.1Hz_to_0.5Hz')
         plot_median_noise(data, 'NEI_1.0Hz_to_2.0Hz')
         plot_median_noise(data, 'NEI_3.0Hz_to_5.0Hz')
