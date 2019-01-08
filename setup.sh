31663464baaea6b973dbe4d34ecf244da7c5cbe3
Adam Anderson <adama@fnal.gov>
2019-01-08 23:07:24 +1300

    Migrate to tabulator version 4.1.5.

--- setup.sh
+++ setup.sh
@@ -16,9 +16,10 @@ mv jquery-ui.css ../css
 wget https://code.jquery.com/jquery-1.12.4.js
 wget https://code.jquery.com/ui/1.12.1/jquery-ui.js
 wget https://momentjs.com/downloads/moment.min.js
-wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/css/tabulator.min.css
+wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/4.1.5/css/tabulator.min.css
 mv tabulator.min.css ../css
-wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/3.1.5/js/tabulator.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/4.1.5/js/tabulator.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/tabulator/4.1.5/js/jquery_wrapper.min.js
 wget https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js
 cd ..
 
