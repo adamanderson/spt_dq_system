9d58ff4e08748f457498763c25de784c85c508f4
Adam Anderson <adama@fnal.gov>
2019-01-25 10:07:08 -0600

    Convert autoprocessing DB file for both development and working copies.

--- convert_autoproc.py
+++ convert_autoproc.py
@@ -1,3 +1,6 @@
 from spt3g.std_processing import status_db
 db = status_db.AutoprocDatabase(filename='/home/centos/spt_dq_system/autoproc.txt', user='centos', host='spt3g-dq.grid.uchicago.edu', read_only=True)
 db.write_sql()
+
+db2 = status_db.AutoprocDatabase(filename='/var/www/spt_dq_system/autoproc.txt', user='centos', host='spt3g-dq.grid.uchicago.edu', read_only=True)
+db2.write_sql()
