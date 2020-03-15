bd938377577de6ade8b5a17229e66a97630029c8
Wei Quan <weiquan@amundsen.grid.uchicago.edu>
2020-03-15 10:45:18 -0500

    Ignore problematic focus quasar maps when plotting them.

--- plot/BeamFWHMandEllipticity.py
+++ plot/BeamFWHMandEllipticity.py
@@ -73,33 +73,34 @@ def BeamFWHMandEllipticity(request):
             params = util.fitting.fit_gaussian2d(tmap)
             (amp, x0, y0, sigma_x, sigma_y, theta, height) = params
             px, py = (int(x0), int(y0))
+
+            fit = util.maths.gaussian2d(*params)(*np.indices(tmap.shape))
+            magic = 2.*np.sqrt(2.*np.log(2)) # sigma to FWHM conversion
+            fwhm_px = magic*np.sqrt(0.5*sigma_x**2 + 0.5*sigma_y**2)
+            fwhm_am = fwhm_px * res / core.G3Units.arcmin
+            ell = np.sqrt(np.abs(sigma_x**2-sigma_y**2) / np.max((sigma_x**2, sigma_y**2)))
+
+            mask = np.ones(fit.shape, dtype=bool)
+            mask[fit/fit.max() > 1e-6] = False # mask source out to -120dB, about 6 sigma
+            noise = np.nanstd(tmap[mask])
+            SNR = amp / noise
+            fitting_success = True
         except:
-            (np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan) = params
-
-        fit = util.maths.gaussian2d(*params)(*np.indices(tmap.shape))
-        magic = 2.*np.sqrt(2.*np.log(2)) # sigma to FWHM conversion
-        fwhm_px = magic*np.sqrt(0.5*sigma_x**2 + 0.5*sigma_y**2)
-        fwhm_am = fwhm_px * res / core.G3Units.arcmin
-        ell = np.sqrt(np.abs(sigma_x**2-sigma_y**2) / np.max((sigma_x**2, sigma_y**2)))
-
-        mask = np.ones(fit.shape, dtype=bool)
-        mask[fit/fit.max() > 1e-6] = False # mask source out to -120dB, about 6 sigma
-        noise = np.nanstd(tmap[mask])
-        SNR = amp / noise
+            fitting_success = False
 
         im = ax.imshow(tmap, cmap=colormap, aspect='equal')
-        ax.contour(fit, [amp/2.], colors=[textcolor], linestyles=['solid'], linewidths=[0.5])
-        ax.text(x=px-0.9*width, y=py-0.6*width, s="FWHM: %.2f\'\ne: %.2f"%(fwhm_am, ell), color=textcolor)
+        if fitting_success:
+            ax.contour(fit, [amp/2.], colors=[textcolor], linestyles=['solid'], linewidths=[0.5])
+            ax.text(x=px-0.9*width, y=py-0.6*width, s="FWHM: %.2f\'\ne: %.2f"%(fwhm_am, ell), color=textcolor)
 
-        px_am = core.G3Units.arcmin / res
-        ct = np.cos(theta)
-        st = np.sin(theta)
-        sgn = 1. if sigma_x < sigma_y else -1.
-        ax.plot([x0 + px_am*ct, x0 - px_am*ct], [y0 + sgn*px_am*st, y0 - sgn*px_am*st], color=textcolor, linewidth=0.5)
-
-        ax.plot([px-0.9*width, px-0.9*width+2*px_am], [py+0.9*width, py+0.9*width], color=textcolor, linewidth=0.5)
-        ax.text(x=px-0.8*width+2*px_am, y=py+0.95*width, s='2\'', color=textcolor)
-        ax.text(x=px+0.45*width, y=py+0.95*width, s=band, color=textcolor)
+            px_am = core.G3Units.arcmin / res
+            ct = np.cos(theta)
+            st = np.sin(theta)
+            sgn = 1. if sigma_x < sigma_y else -1.
+            ax.plot([x0 + px_am*ct, x0 - px_am*ct], [y0 + sgn*px_am*st, y0 - sgn*px_am*st], color=textcolor, linewidth=0.5)
+            ax.plot([px-0.9*width, px-0.9*width+2*px_am], [py+0.9*width, py+0.9*width], color=textcolor, linewidth=0.5)
+            ax.text(x=px-0.8*width+2*px_am, y=py+0.95*width, s='2\'', color=textcolor)
+            ax.text(x=px+0.45*width, y=py+0.95*width, s=band, color=textcolor)
 
         ax.set_xticklabels([])
         ax.set_yticklabels([])
@@ -107,17 +108,20 @@ def BeamFWHMandEllipticity(request):
         ax.set_yticks([])
 
         divider = make_axes_locatable(ax)
-        if orientation == 'vertical':
-            cax = divider.append_axes('right', size='5%', pad=0.05)
-            cb = fig.colorbar(im, cax=cax, orientation='vertical')
-            cb.ax.yaxis.set_ticks_position('right')
-            cb.ax.yaxis.set_label_position('right')
+        if fitting_success:
+            if orientation == 'vertical':
+                cax = divider.append_axes('right', size='5%', pad=0.05)
+                cb = fig.colorbar(im, cax=cax, orientation='vertical')
+                cb.ax.yaxis.set_ticks_position('right')
+                cb.ax.yaxis.set_label_position('right')
+            else:
+                cax = divider.append_axes('bottom', size='5%', pad=0.05)
+                cb = fig.colorbar(im, cax=cax, orientation='horizontal')
+                cb.ax.xaxis.set_ticks_position('bottom')
+                cb.ax.xaxis.set_label_position('bottom')
+            cb.set_label("SNR")
         else:
-            cax = divider.append_axes('bottom', size='5%', pad=0.05)
-            cb = fig.colorbar(im, cax=cax, orientation='horizontal')
-            cb.ax.xaxis.set_ticks_position('bottom')
-            cb.ax.xaxis.set_label_position('bottom')
-        cb.set_label("SNR")
+            ax.text(x=0.5, y=0.5, s=band+'\nSomething\nwent\nwrong...', transform=ax.transAxes, ha='center', va='center')
 
     if benchpos is not None:
         vpos = pointing.focus.bench2optical(benchpos['x4'], benchpos['y1'], benchpos['z6'])
